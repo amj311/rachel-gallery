eaa5bf3d243849ea59022ea6eb573ff3ee7650e0
Arthur <amjudd315@gmail.com>
2024-05-20 23:59:51 -0600

    server url

--- app/src/services/request.js
+++ app/src/services/request.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const apiUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:5000/api' : 'https://hypno-club.onrender.com/api';
+export const apiUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:5000/api' : 'https://rachel-gallery-server.onrender.com/api';
 
 const request = axios.create({
 	baseURL: apiUrl,
