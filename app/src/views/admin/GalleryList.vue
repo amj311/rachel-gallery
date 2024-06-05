4c4c5eedb699af0a41d36bd4d983d77e13d27068
Arthur <amjudd315@gmail.com>
2024-06-04 21:58:17 -0600

    gallery list style

--- app/src/views/admin/GalleryList.vue
+++ app/src/views/admin/GalleryList.vue
@@ -41,35 +41,36 @@ async function createNewGallery() {
 
 		<div class="gallery-grid">
 			<template v-for="gallery in state.galleries" :key="gallery.id" >
-				<Card size="small" :style="{ maxWidth: '400px', zoom: .8 }" class="overflow-hidden cursor-pointer" @click="router.push(`/admin/galleries/${gallery.id}`)">
-					<template #header><div class="cover-small"><GalleryCover :gallery="gallery" :preview="true" forceMode="desktop" /></div></template>
-					<template #title>{{ gallery.name || 'Untitled' }}</template>
-					<template #subtitle>{{ gallery.clientName || gallery.clientEmail }}</template>
-					
-					<template #content>
-						<div class="flex align-items-center gap-3">
-							<div class="flex align-items-center gap-1" :style="{ color: visibilityOptions[gallery.visibility].color }">
-								<i :class="visibilityOptions[gallery.visibility].icon" />
-								{{ visibilityOptions[gallery.visibility].label }}
+				<div>
+					<Card size="small" :style="{ width: '100%', zoom: .8 }" class="overflow-hidden cursor-pointer" @click="router.push(`/admin/galleries/${gallery.id}`)">
+						<template #header><div class="cover-small"><GalleryCover :gallery="gallery" :preview="true" forceMode="desktop" /></div></template>
+						<template #title>{{ gallery.name || 'Untitled' }}</template>
+						<template #subtitle>{{ gallery.clientName || gallery.clientEmail }}</template>
+						
+						<template #content>
+							<div class="flex align-items-center gap-3">
+								<div class="flex align-items-center gap-1" :style="{ color: visibilityOptions[gallery.visibility].color }">
+									<i :class="visibilityOptions[gallery.visibility].icon" />
+									{{ visibilityOptions[gallery.visibility].label }}
+								</div>
+
+								<div class="flex-grow-1"></div>
+
+
+								<div class="flex align-items-center gap-1">
+									<i class="pi pi-images" />
+									<span>{{ gallery.sections.reduce((t, s) => t + s._count.photos, 0) }}</span>
+								</div>
+
+
+								<div v-if="gallery.date" class="flex align-items-center gap-1">
+									<i class="pi pi-calendar" />
+									{{ dayjs(gallery.date).format('MMM DD, \'YY') }}
+								</div>
 							</div>
-
-							<div class="flex-grow-1"></div>
-
-
-							<div class="flex align-items-center gap-1">
-								<i class="pi pi-images" />
-								<span>{{ gallery.sections.reduce((t, s) => t + s._count.photos, 0) }}</span>
-							</div>
-
-
-							<div v-if="gallery.date" class="flex align-items-center gap-1">
-								<i class="pi pi-calendar" />
-								{{ dayjs(gallery.date).format('MMM DD, \'YY') }}
-							</div>
-						</div>
-					</template>
-								
-				</Card>
+						</template>
+					</Card>
+				</div>
 			</template>
 		</div>
 	</div>
@@ -79,14 +80,16 @@ async function createNewGallery() {
 
 .gallery-grid {
 	display: grid;
-	grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+	grid-template-columns: repeat(auto-fill, minmax(17rem, 1fr));
 	gap: 20px;
+	justify-content: center;
 }
 
 .cover-small {
 	font-size: 2em;
-	zoom: .2;
-	height: 900px;
+	zoom: .18;
+	width: 100%;
+	aspect-ratio: 1.8;
 	pointer-events: none;
 	user-select: none;
 }
