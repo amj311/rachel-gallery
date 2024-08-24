4297d049c3b2629deb99426df3336722b5f5cb7c
Arthur <amjudd315@gmail.com>
2024-08-24 15:20:04 -0600

    find server host

--- app/src/services/requestInterceptor.ts
+++ app/src/services/requestInterceptor.ts
@@ -1,7 +1,27 @@
 import { AuthService } from "./authService";
 import request from "./request";
+import axios from "axios";
+
+let serverHost = '';
+
+async function discoverHost() {
+	if (process.env.NODE_ENV === 'development') {
+		serverHost = 'http://localhost:5000';
+	}
+	else {
+		console.log("LOADING IP ADDRESS")
+		const res = await axios.get('http://104.37.250.80:31513');
+		console.log(res.data);
+		serverHost = res.data;
+	}
+}
 
 request.interceptors.request.use(async (config) => {
+	// Find base url
+	if (!serverHost) {
+		await discoverHost();
+	}
+
 	const token = await AuthService.getToken();
 	if (token) {
 		config.headers.Authorization = token;
@@ -10,7 +30,7 @@ request.interceptors.request.use(async (config) => {
 });
 
 request.interceptors.response.use(null, (error) => {
-	AuthService.info.push("SERVER ERROR: " + error.message);
+	
 	if (error.isAxiosError && error.response?.status === 401) {
 		console.log("Received unauthenticated response. Logging out");
 		AuthService.signOut();
