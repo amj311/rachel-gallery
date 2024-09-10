06f4e72a5aff3d7fd19d8e8af0c7a3e2f154cb99
Arthur <amjudd315@gmail.com>
2024-09-10 13:59:53 -0600

    gallery sorting

--- app/src/views/admin/GalleryList.vue
+++ app/src/views/admin/GalleryList.vue
@@ -1,18 +1,22 @@
 <script setup lang="ts">
 import { useRouter, RouterView } from 'vue-router';
-import { onBeforeMount, reactive } from 'vue';
+import { computed, onBeforeMount, reactive } from 'vue';
 import request from '@/services/request';
 import GalleryCover from '@/components/GalleryCover.vue';
 import Card from 'primevue/card';
 import dayjs from 'dayjs';
 import Button from 'primevue/button';
 import { visibilityOptions } from '@/utils/visibilityOptions';
+import DropdownMenu from '@/components/DropdownMenu.vue';
 
 const router = useRouter();
 
 const state = reactive({
 	isLoading: true,
 	galleries: [] as any[],
+	sortBy: 'Date',
+	sortOrder: 'desc',
+	clientId: 'all' as any,
 });
 
 onBeforeMount(async () => {
@@ -26,21 +30,64 @@ async function createNewGallery() {
 	router.push(`/admin/galleries/${data.data.id}`);
 }
 
+const sortOptions = [
+	{ label: 'Date', command() { state.sortBy = 'Date'; } },
+	{ label: 'Name', command() { state.sortBy = 'Name'; } },
+];
+
+function toggleSortOrder() {
+	if (state.sortOrder === 'desc') {
+		state.sortOrder = 'asc';
+	} else {
+		state.sortOrder = 'desc';
+	}
+}
+
+const clientOptions = computed(() => ([
+	{ label: 'All Clients', value: 'all', command() { state.clientId = 'all'; } },
+	...(new Map(state.galleries.map(g => [g.clientId, { label: g.Client?.name || 'No Client', value: g.clientId, command() { state.clientId = g.clientId; } }]))).values(),
+]));
+const clientLabel = computed(() => clientOptions.value.find(o => o.value === state.clientId)?.label || 'All Clients');
+
+const sorters = {
+	'Name': (a: any, b: any) => a.name.localeCompare(b.name),
+	'Date': (a: any, b: any) => dayjs(a.date).diff(dayjs(b.date), 'day'),
+}
+
+const sortedGalleries = computed(() => {
+	const sorted = [...state.galleries];
+	sorted.sort(sorters[state.sortBy]);
+	if (state.sortOrder === 'desc') {
+		sorted.reverse();
+	}
+	return state.clientId === 'all' ? sorted : sorted.filter(g => g.clientId === state.clientId);
+})
 </script>
 
 
 <template>
 	<div>
-		<div class="flex align-items-center gap-3 mt-2 mb-4">
+		<div class="flex align-items-center flex-wrap gap-3 mt-2 mb-4">
 			<h1>Your Galleries</h1>
 			<div class="flex-grow-1"></div>
+			<div style="zoom: .9">
+				<DropdownMenu :model="clientOptions">
+					<Button text class="gap-2" icon="pi pi-user" :label="clientLabel" />
+				</DropdownMenu>
+				<Button text class="gap-2">
+					<i @click="toggleSortOrder" :class="`pi pi-sort-amount-${ state.sortOrder === 'desc' ? 'up' : 'down'}`" />
+					<DropdownMenu :model="sortOptions">
+						<div>{{ state.sortBy }}</div>
+					</DropdownMenu>
+				</Button>
+			</div>
 			<Button @click="createNewGallery" severity="primary">&plus; New Gallery</Button>
 		</div>
 
 		<div v-if="state.isLoading"><i class="pi pi-spinner pi-spin" /> Loading...</div>
 
 		<div class="gallery-grid">
-			<template v-for="gallery in state.galleries" :key="gallery.id" >
+			<template v-for="gallery in sortedGalleries" :key="gallery.id" >
 				<div>
 					<Card size="small" :style="{ width: '100%', zoom: .8 }" class="overflow-hidden cursor-pointer" @click="router.push(`/admin/galleries/${gallery.id}`)">
 						<template #header><div class="cover-small"><GalleryCover :gallery="gallery" :preview="true" forceMode="desktop" /></div></template>
