2bc6e89276e4de1694f1cff140ea6363bdeeb0f1
Arthur Judd <amjudd315@gmail.com>
2025-01-27 00:13:28 -0700

    build and prune label

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,9 @@ if [[ -f .env.deploy ]]; then
 fi
 
 # Run docker compose on host, force build and recreate
-sudo DOCKER_HOST=ssh://${SSH_USER}@${SSH_HOST} docker compose -f docker-compose-prod.yml up -d --build --force-recreate
+sudo DOCKER_HOST=ssh://${SSH_USER}@${SSH_HOST} docker-compose -f docker-compose-prod.yml up -d --build --force-recreate
 # Post build cleanup
-sudo DOCKER_HOST=ssh://${SSH_USER}@${SSH_HOST} docker system prune -f
+sudo DOCKER_HOST=ssh://${SSH_USER}@${SSH_HOST} docker system prune -f --filter "label=com.docker.compose.project=gallery"
 
 echo "Finished!"
 exit 0
