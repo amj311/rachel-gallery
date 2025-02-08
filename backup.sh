cfc639cd86b990b09b74e54bab2b4d388366782e
Arthur Judd <amjudd315@gmail.com>
2025-02-07 23:42:05 -0700

    backup with deploy

--- backup.sh
+++ backup.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-# See https://www.howtogeek.com/devops/how-to-back-up-your-docker-volumes/
+BACKUP_DIR="${HOME}/backups"
+PROJECT="rachel-gallery"
+VOLUME="gallery-db"
+FILE_NAME="${PROJECT}_${VOLUME}_$(date +%Y-%m-%d_%H-%M-%S).tar"
+
+echo -e "\nCreating backup for volume ${VOLUME}..."
 
 # Start a temporary container to back up the "mysql_data" volume
-docker run --rm --volumes-from gallery-db -v $PWD:/backup-dir ubuntu tar cvf /backup-dir/gallery-db-backup.tar /var/lib/postgresql/data
+# See https://www.howtogeek.com/devops/how-to-back-up-your-docker-volumes/
+sudo docker run --rm --volumes-from $VOLUME -v $BACKUP_DIR:/backup-dir ubuntu tar cvf /backup-dir/${FILE_NAME} /var/lib/postgresql/data > /dev/null
+
+echo -e "\nCreated backup for volume '${VOLUME}' at ${BACKUP_DIR}/${FILE_NAME}. Check above for any errors."
--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,16 @@ if [[ -f .env.deploy ]]; then
 	. .env.deploy
 fi
 
-# Run docker compose on host, force build and recreate
-sudo DOCKER_HOST=ssh://${SSH_USER}@${SSH_HOST} docker-compose -f docker-compose-prod.yml up -d --build --force-recreate
-# Post build cleanup
-sudo DOCKER_HOST=ssh://${SSH_USER}@${SSH_HOST} docker system prune -f --filter "label=com.docker.compose.project=gallery"
+cat 'backup.sh' | ssh -o ExitOnForwardFailure=yes ${SSH_USER}@${SSH_HOST}
 
-echo "Finished!"
+# Run docker compose on host, force build and recreate
+echo -e "\n\nDeploying new docker image...\n"
+sudo DOCKER_HOST=ssh://${SSH_USER}@${SSH_HOST} docker-compose -f docker-compose-prod.yml up -d --build --force-recreate > /dev/null
+
+
+echo -e "\nCleaning up...\n"
+# Post build cleanup
+sudo DOCKER_HOST=ssh://${SSH_USER}@${SSH_HOST} docker system prune -f --filter "label=com.docker.compose.project=gallery" > /dev/null
+
+echo -e "Finished!"
 exit 0
